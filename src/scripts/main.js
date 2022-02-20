2eeffb7098c9e0b3ecc81f98f554c83edea2a923
rushayhays <rushayhays@outlook.com>
2022-02-20 17:56:15 -0600

    I think I got tractor to work correctly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,6 +26,24 @@ import { createCorn } from "./seeds/corn.js"
 const cornSeed = createCorn()
 console.log(cornSeed)
 
+//test potato
+import { createPotato } from "./seeds/potato.js"
+
+const potatoSeed = createPotato()
+console.log(potatoSeed)
+
+//test soybean
+import { createSoybean } from "./seeds/soybean.js"
+
+const soybeanSeed = createSoybean()
+console.log(soybeanSeed)
+
+//test sunflower
+import { createSunflower } from "./seeds/sunflower.js"
+
+const sunflowerSeed = createSunflower()
+console.log(sunflowerSeed)
+
 //here we run the field test
 import { addPlant } from "./field.js";
 import { usePlants } from "./field.js";
